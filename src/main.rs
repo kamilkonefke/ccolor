006b1048ee65c7095355892eb80f437dc4bfe296
Catoo <86194812+kamilkonefke@users.noreply.github.com>
2024-04-04 10:21:27 +0200

    rewritten code

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,52 @@
 use clap::Parser;
+use colored::Colorize;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about)]
-
 pub struct Args {
+    /// Get color information.
     #[arg(short, long)]
-    pub hex: String,
+    info: String,
+}
+
+pub struct Color {
+    r: i32,
+    g: i32,
+    b: i32,
 }
 
 fn main() {
     let args = Args::parse();
 
-    println!("Hex: #{}", args.hex);
-    println!("R: {}, G: {}, B: {}", hex_to_rgb(args.hex.clone())[0], hex_to_rgb(args.hex.clone())[1], hex_to_rgb(args.hex)[2]); // Yup... I know...
+    let output = hex_to_rgb(&args.info);
+
+    println!("Hex: #{}", &args.info);
+
+    // RGB output
+    print!(" R: {}", output.r.to_string().red());
+    print!(" G: {}", output.g.to_string().green());
+    print!(" B: {}", output.b.to_string().blue());
 }
 
-fn hex_to_dec(hex: &str) -> i32 {
-    i32::from_str_radix(hex, 16).expect("err")
+fn hex_to_rgb(hex: &str) -> Color {
+
+    let color = split_hex(hex);
+
+    Color {
+        r: i32::from_str_radix(color[0].as_str(), 16).expect("err"),
+        g: i32::from_str_radix(color[1].as_str(), 16).expect("err"),
+        b: i32::from_str_radix(color[2].as_str(), 16).expect("err"),
+    }
 }
 
-fn hex_to_rgb(hex: String) -> [i32; 3] {
-
-    let r_h = hex.split_at(2);
-    let g_h = r_h.1.split_at(2);
-    let b_h = g_h.1.split_at(2);
-
-    let rgb_array = [
-        hex_to_dec(r_h.0),
-        hex_to_dec(g_h.0),
-        hex_to_dec(b_h.0),
-    ];
-
-    rgb_array
+fn hex_to_float(hex: &str) -> Color {
+    todo!();
 }
 
-fn hex_to_frgb(hex: String) -> String {
-    todo!()
+fn split_hex(hex: &str) -> Vec<String> {
+    vec![
+        hex.get(0..2).unwrap().to_string(),
+        hex.get(2..4).unwrap().to_string(),
+        hex.get(4..6).unwrap().to_string(),
+    ]
 }
